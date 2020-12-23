f0d4618c0833c6709fa76404c1f8b10938c96df5
dlegland <david.legland@inrae.fr>
2020-12-23 09:34:14 +0100

    fix UnfoldVectorImage.m (unfold method now returns Table instance)

--- ImageM/+imagem/+actions/+image/UnfoldVectorImage.m
+++ ImageM/+imagem/+actions/+image/UnfoldVectorImage.m
@@ -44,9 +44,16 @@ methods
             return;
         end
         
+        % default table name
+        tableName = 'Channels';
+        if ~isempty(img.Name)
+            tableName = [img.Name '-Channels'];
+        end
+        
         % creates a new dialog, and populates it with some fields
         gd = imagem.gui.GenericDialog('Unfold Vector Image');
-        addChoice(gd, 'Plot Type: ', {'line', 'bar', 'stem', 'stairSteps'}, 'line');
+        addTextField(gd, 'Table Name: ', tableName);
+        addChoice(gd, 'Channel Plot Type: ', {'line', 'bar', 'stem', 'stairSteps'}, 'line');
         addCheckBox(gd, 'Create Coords Table', false);
         
         % displays the dialog, and waits for user
@@ -57,58 +64,41 @@ methods
         end
         
         % parse the user inputs
+        tableName = getNextString(gd);
         type = getNextString(gd);
         createCoordsTable = getNextBoolean(gd);
         
         % unfold the table
-        [data, colNames, coords] = unfold(img);
-        tab = Table(data, colNames);
+        [tab, coords] = unfold(img);
+        tab.Name = tableName;
         nc = size(tab, 2);
         tab.PreferredPlotTypes = repmat({type}, 1, nc);
-        
-        % create table axis that corresponds to image axis
-        indC = findChannelAxes(img);
-        imAxis = img.Axes{indC};
-        if isa(imAxis, 'image.axis.NumericalAxis')
-            values = indexToPosition(imAxis, 1:nc);
-            axis = table.axis.NumericalAxis(imAxis.Name, values);
-            tab.Axes{2} = axis;
-            
-        elseif isa(imAxis, 'image.axis.CategoricalAxis')
-            axis = table.axis.CategoricalAxis(imAxis.Name, imAxis.Names);
-            tab.Axes{2} = axis;
-            
-        else
-            warning(['Unknown type of axis in image: ' class(imAxis)]);
-        end
-            
         
         % create table viewer
         [newFrame, newDoc] = createTableFrame(frame.Gui, tab, frame); %#ok<ASGLU>
         newDoc.ImageSize = size(img, 1:ndims(img));
 
-        % add to history
-        string = sprintf('[tab, names, coords] = unfold(%s);\n', doc.Tag);
-        addToHistory(frame, string);
-        string = sprintf('%s = Table(tab, names});\n', newDoc.Tag);
-        addToHistory(frame, string);
-        
         % also create a table for coordinates if requested
         if createCoordsTable
             if ndims(img) == 2 %#ok<ISMAT>
                 coordNames = {'x', 'y'};
-                coordStr = '{''x'', ''y''}';
             elseif ndims(img) == 3
                 coordNames = {'x', 'y', 'z'};
-                coordStr = '{''x'', ''y'', ''z''}';
             end
             coordsTable = Table(coords, coordNames);
             [coordsFrame, coordsDoc] = createTableFrame(frame.Gui, coordsTable); %#ok<ASGLU>
-            
-            % add to history
-            string = sprintf('%s = Table(coords, %s});\n', coordsDoc.Tag, coordStr);
+        end
+        
+        % add to history
+        if createCoordsTable
+            string = sprintf('[%s, %s] = unfold(%s);\n', ...
+                newDoc.Tag, coordsDoc.Tag, doc.Tag);
+            addToHistory(frame, string);
+        else
+            string = sprintf('%s = unfold(%s);\n', newDoc.Tag, doc.Tag);
             addToHistory(frame, string);
         end
+        
     end
     
 end % end methods
